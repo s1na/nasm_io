065bd4ef44d516459b4e69e6e0abcf0083084488
Sina Mahmoodi <itz.s1na@gmail.com>
2012-11-07 12:26:06 +0330

    trying to determine str length

--- io.asm
+++ io.asm
@@ -4,11 +4,11 @@
 ;               output string, string_length
 ;               input  buffer, buffer_length
 
-%macro          output          2
+%macro          output          1
                 
-                mov             ecx, %1
-                mov             edx, %2
+                mov             arg, %1
                 call            print
+                sub             esp, [arg_len]
 
 %endmacro
 
@@ -20,14 +20,45 @@
 
 %endmacro
 
+section .bss
+
+arg             resb            50
+arg_len         equ             $ - arg
+index           resb            1
+
+
 section .text
 
 
 print:
+                push            eax
+                push            ebx
+                push            ecx
+                push            edx
+
+                mov             index, -1
+                call            get_string
+
+                mov             ecx, arg
+                mov             edx, arg_len
                 mov             eax, 4
                 mov             ebx, 1
                 int             80h
+
+                pop             edx
+                pop             ecx
+                pop             ebx
+                pop             eax
                 ret
+
+get_string:
+                inc             index
+                cmp             [arg + index], 0
+                jne             get_string
+
+                mov             arg_len, index
+
+
 
 input_:
                 mov             eax, 3
